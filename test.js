f4b92f958e016189a18065e6a626e57bdacc0bbb
Paweł Bihun <pawelbihun@gmail.com>
2021-02-07 23:21:34 +0100

    made a hamburger menu + added new test file for js

---
+++ test.js
@@ -0,0 +1,62 @@
+const firstName = "Paweł";
+const age = 30;
+
+//const heading = document.querySelector('.main-header--js');
+//heading.innerHTML = '<h1>Header zmieniony przez JS<h1>';
+
+const newSection = document.querySelector('.section--js');
+newSection.innerHTML = `<article class="article">
+                    <h3 class="article__title">Java Script - Test selektora</h3>
+                    <p class="article__description">
+                    JavaScript (w skrócie JS) – skryptowy język programowania, stworzony
+                    przez firmę Netscape, najczęściej stosowany na stronach
+                    internetowych. Twórcą JavaScriptu jest Brendan Eich[4]. W połowie
+                    lat 90. XX wieku organizacja ECMA wydała na podstawie JavaScriptu
+                    standard języka skryptowego o nazwie ECMAScript, aktualnie
+                    rozwijaniem tego standardu zajmuje się komisja TC39.
+                    </p>`;
+
+
+function greetOld (age, firstName){
+    console.log(`Hi!, my name is ${firstName} and I am ${age} years old !`);
+}
+greetOld(20, 'Paul')
+
+const greet = (age, firstName) => {
+    console.log(`Hi!, my name is ${firstName} and I am ${age} years old !`);
+}
+greet(24, 'Adam')
+
+function createContent(querySelectorContent, content){
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content; 
+}
+createContent('.article__description--js', 'Ten znacznik został uzupełniony java scriptem :) - sprawdź czy istnieje w źródle strony - ^U');
+
+const deathStar = {
+    diamnater: 120000,
+    fire: (target) => {
+        console.log(`${target} destroyed !`)
+    },
+    isOperating: true,
+    name: 'Death Star',
+    levels: 357,
+    population: 10000,
+    isLightOn: true,
+    commander: {
+        name: 'Darth Vader',
+        age: 44
+    }
+}
+
+console.log(`The commander of ${deathStar.name} is ${deathStar.commander.name}.`);
+deathStar.fire('rebel space ship');
+
+
+// hamburger menu 
+const menu = document.querySelector('.hamburger-menu--js');
+menu.addEventListener('click', (e) => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--close');
+    nav.classList.toggle('navigation--add-top-padding');
+})
