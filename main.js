9836198b41739ab44a970f7e63f3b3c888241d64
Paweł Bihun <pawelbihun@gmail.com>
2021-02-11 21:47:34 +0100

    fixed section added with js, code refactored

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ menu.addEventListener('click', (e) => {
     nav.classList.toggle('navigation--close');
     nav.classList.toggle('navigation--add-top-padding');
 })
+
+// uzupełnieni sekcji javascriptem
+const newSection = document.querySelector('.section--js');
+newSection.innerHTML = `<article class="article">
+                    <h3 class="article__title">Java Script - Test selektora</h3>
+                    <p class="article__description">
+                    JavaScript (w skrócie JS) – skryptowy język programowania, stworzony
+                    przez firmę Netscape, najczęściej stosowany na stronach
+                    internetowych. Twórcą JavaScriptu jest Brendan Eich[4]. W połowie
+                    lat 90. XX wieku organizacja ECMA wydała na podstawie JavaScriptu
+                    standard języka skryptowego o nazwie ECMAScript, aktualnie
+                    rozwijaniem tego standardu zajmuje się komisja TC39.
+                    </p>`;
--- test.js
+++ test.js
@@ -1,22 +1,6 @@
 const firstName = "Paweł";
 const age = 30;
 
-//const heading = document.querySelector('.main-header--js');
-//heading.innerHTML = '<h1>Header zmieniony przez JS<h1>';
-
-const newSection = document.querySelector('.section--js');
-newSection.innerHTML = `<article class="article">
-                    <h3 class="article__title">Java Script - Test selektora</h3>
-                    <p class="article__description">
-                    JavaScript (w skrócie JS) – skryptowy język programowania, stworzony
-                    przez firmę Netscape, najczęściej stosowany na stronach
-                    internetowych. Twórcą JavaScriptu jest Brendan Eich[4]. W połowie
-                    lat 90. XX wieku organizacja ECMA wydała na podstawie JavaScriptu
-                    standard języka skryptowego o nazwie ECMAScript, aktualnie
-                    rozwijaniem tego standardu zajmuje się komisja TC39.
-                    </p>`;
-
-
 function greetOld (age, firstName){
     console.log(`Hi!, my name is ${firstName} and I am ${age} years old !`);
 }
