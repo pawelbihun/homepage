9d64a74dd159d4f103171acbcd1e2f88d66fde32
Paweł Bihun <pawelbihun@gmail.com>
2021-02-16 23:11:18 +0100

    game check if target field is empty

--- game.js
+++ game.js
@@ -4,32 +4,45 @@ const CROSS = '<i class="fas fa-times"></i>';
 const board = document.querySelectorAll(".board__field");
 let round = 1;
 
+const boardShadow = [];
+
+// game logic
 board.forEach((field) =>
   field.addEventListener("click", (event) => {
     const name = event.target.className;
     const target = `.${name.substring(13)}`;
-    let player = round % 2;
-    if (player === 0) {
-      addSymbol(target, CROSS);
+
+    if (isFieldEmpty(target)) {
+      let player = round % 2;
+      if (player === 0) {
+        addSymbol(target, CROSS);
+      } else {
+        addSymbol(target, CIRCLE);
+      }
+      round++;
     } else {
-      addSymbol(target, CIRCLE);
+      alert(`To pole jest zajęte przez przeciwnika.\nSpróbuj inne.`)
     }
-    round++;
-    showPlayer(round);
+
+
+    changePlayer(round);
   })
 );
-
+// dokończyć
+function isFieldEmpty(element) {
+  let pattern = /\.board__field.*/;
+  return pattern.test(element);
+}
 function addSymbol(querySelectorContent, content) {
   const element = document.querySelector(querySelectorContent);
   element.innerHTML = content;
 }
-function showPlayer(roundNumber) {
+function changePlayer(roundNumber) {
   const info = document.querySelector(".player");
   let player = roundNumber % 2;
   if (player === 0) {
     info.innerHTML = `Aktuany gracz: ${CROSS}`;
-  }
-  else {
-    info.innerHTML = `Aktuany gracz: ${CIRCLE}`
+  } else {
+    info.innerHTML = `Aktuany gracz: ${CIRCLE}`;
   }
 }
